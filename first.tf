12cd7e3025fa673706e65ffcaa0954dc0e098ae9
Gaurav9540 <balapawar9146@gmail.com>
2025-05-10 17:46:04 +0530

    adds file

---
+++ first.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1" # or your preferred region
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0e35ddab05955cf57"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
