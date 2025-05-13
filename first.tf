79f7023e837a72784f552fdb8c71041b7751f652
Gaurav9540 <balapawar9146@gmail.com>
2025-05-13 12:19:11 +0530

    make changes for VPC creation

--- first.tf
+++ first.tf
@@ -58,7 +58,7 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw
+    gateway_id = aws_internet_gateway.gw.id
   }
   tags = {
     Name = "public-rt"
