411740a319bcc5d2a086f7237085aa45790979c6
Gaurav9540 <balapawar9146@gmail.com>
2025-05-13 13:06:56 +0530

    separate the files

--- first.tf
+++ first.tf
@@ -1,84 +1,84 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "s3-for-terraform-demo"
-#     key    = "new-key.pem"
-#     region = "ap-south-1"
+# # terraform {
+# #   backend "s3" {
+# #     bucket = "s3-for-terraform-demo"
+# #     key    = "new-key.pem"
+# #     region = "ap-south-1"
+# #   }
+# # }
+
+# provider "aws" {
+#   region = "ap-south-1" 
+# }
+
+# variable "instance_type" {
+#   description = "This is the instance type for the demo EC2 instance"
+#   type        = string
+#   default     = "t3.micro"
+# }
+
+# output "instance_ip_addr" {
+#   value = aws_instance.web.private_ip
+# }
+
+
+# resource "aws_vpc" "main" {
+#   cidr_block = "10.0.0.0/20"
+#   tags = {
+#     Name = "myvpc"
 #   }
 # }
 
-provider "aws" {
-  region = "ap-south-1" 
-}
+# resource "aws_subnet" "public" {
+#   vpc_id     = aws_vpc.main.id
+#   cidr_block = "10.0.1.0/24"
+#   map_public_ip_on_launch = true
+#   tags = {
+#     Name = "pub-subnet"
+#   }
+# }
 
-variable "instance_type" {
-  description = "This is the instance type for the demo EC2 instance"
-  type        = string
-  default     = "t3.micro"
-}
+# resource "aws_subnet" "private" {
+#   vpc_id     = aws_vpc.main.id
+#   cidr_block = "10.0.2.0/24"
 
-output "instance_ip_addr" {
-  value = aws_instance.web.private_ip
-}
+#   tags = {
+#     Name = "pvt-subnet"
+#   }
+# }
 
+# resource "aws_internet_gateway" "gw" {
+#   vpc_id = aws_vpc.main.id
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/20"
-  tags = {
-    Name = "myvpc"
-  }
-}
+#   tags = {
+#     Name = "myIGW"
+#   }
+# }
 
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "pub-subnet"
-  }
-}
+# resource "aws_route_table" "public" {
+#   vpc_id = aws_vpc.main.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.gw.id
+#   }
+#   tags = {
+#     Name = "public-rt"
+#   }
+# }
 
-resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-
-  tags = {
-    Name = "pvt-subnet"
-  }
-}
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "myIGW"
-  }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-  tags = {
-    Name = "public-rt"
-  }
-}
-
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
+# resource "aws_route_table_association" "public_assoc" {
+#   subnet_id = aws_subnet.public.id
+#   route_table_id = aws_route_table.public.id
+# }
 
 
-resource "aws_instance" "web" {
-  ami  = "ami-0e35ddab05955cf57"
-  instance_type = var.instance_type
-  subnet_id = aws_subnet.public.id
-  associate_public_ip_address = true
-  key_name = "new-key"
+# resource "aws_instance" "web" {
+#   ami  = "ami-0e35ddab05955cf57"
+#   instance_type = var.instance_type
+#   subnet_id = aws_subnet.public.id
+#   associate_public_ip_address = true
+#   key_name = "new-key"
 
-  tags = {
-    Name = "MyWebServer"
-  }
-}
+#   tags = {
+#     Name = "MyWebServer"
+#   }
+# }
