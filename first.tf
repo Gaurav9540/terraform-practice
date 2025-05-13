d9fe33f918e9f9f18aa2ada47c95bd6721cdf8e0
Gaurav9540 <balapawar9146@gmail.com>
2025-05-13 12:12:24 +0530

    make changes for VPC creation

--- first.tf
+++ first.tf
@@ -21,9 +21,62 @@ output "instance_ip_addr" {
 }
 
 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/20"
+  tags = {
+    Name = "myvpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "pub-subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "pvt-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "myIGW"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw
+  }
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+
 resource "aws_instance" "web" {
   ami  = "ami-0e35ddab05955cf57"
   instance_type = var.instance_type
+  subnet_id = aws_subnet.public.id
+  associate_public_ip_address = true
+  key_name = "new-key.pem"
 
   tags = {
     Name = "MyWebServer"
