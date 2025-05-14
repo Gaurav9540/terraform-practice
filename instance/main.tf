44d5e63e74c76b78fa76a71fc9c4f1f0b38668fb
Gaurav9540 <balapawar9146@gmail.com>
2025-05-14 13:09:20 +0530

    changes in modules

--- instance/main.tf
+++ instance/main.tf
@@ -1,10 +1,8 @@
 resource "aws_instance" "web" {
   ami  = var.ami_id
   instance_type = var.instance_type
-  subnet_id = aws_subnet.public.id
   associate_public_ip_address = true
   key_name = var.key_name
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
     Name = var.instance_name
