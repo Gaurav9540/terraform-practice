31efb469e41513ed6405bb80443aa04b5464e7cf
Gaurav9540 <balapawar9146@gmail.com>
2025-05-14 12:57:31 +0530

    changes in modules

--- instance/main.tf
+++ instance/main.tf
@@ -1,30 +1,3 @@
-resource "aws_security_group" "web_sg" {
-  name        = "web_sg"
-  description = "Allow HTTP and SSH"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "my_sg"
-  }
-}
-
-
 resource "aws_instance" "web" {
   ami  = var.ami_id
   instance_type = var.instance_type
--- variable.tf
+++ variable.tf
@@ -32,8 +32,4 @@ variable "instance_name" {
   default = "INSTANCE-1"
 }
 
-output "instance_ip_addr" {
-  value = aws_instance.web.private_ip
-}
-
 
