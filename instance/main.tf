eabf9aff95939eec62c188f620ec0df6be7d349b
Gaurav9540 <balapawar9146@gmail.com>
2025-05-16 16:22:31 +0530

    added private instace

--- instance/main.tf
+++ instance/main.tf
@@ -14,8 +14,8 @@ resource "aws_instance" "web" {
 resource "aws_instance" "private_web" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  subnet_id              = var.private_subnet_id   # Use a private subnet here
-  associate_public_ip_address = false              # Important: no public IP
+  subnet_id              = var.private_subnet_id   
+  associate_public_ip_address = false              
   key_name               = var.key_name
   vpc_security_group_ids = var.security_group_ids
 
