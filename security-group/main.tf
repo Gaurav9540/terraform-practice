fde97fe823a8c8729822409d579640737922a706
Gaurav9540 <balapawar9146@gmail.com>
2025-05-16 16:49:40 +0530

    added private instace

--- security-group/main.tf
+++ security-group/main.tf
@@ -3,6 +3,22 @@ resource "aws_security_group" "mysg" {
   description = var.desc
   vpc_id = var.vpc_id
 
+  ingress {
+    description      = "Allow SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"] 
+  }
+
+  ingress {
+    description      = "Allow HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = var.name
   }
